74616f965a91a4008844e6aa1a105c418db30435
anneKoethke <anne.koethke@stud.uni-regensburg.de>
2020-10-13 11:48:03 +0200

    added global styles and change App a little

---
+++ res/style/global.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#2c3e50', // flatUiColors Midnight Blue
+    margin: 10,
+  },
+});
